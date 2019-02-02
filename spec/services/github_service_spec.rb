100719eae40cecf55f145a2c532786fadeaae2ea
Leah Miller <lkmiller.87@gmail.com>
2019-02-02 14:58:39 -0700

    Removes hard-coded token into ENV[actual]

--- spec/services/github_service_spec.rb
+++ spec/services/github_service_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe GithubService do
   it 'exists', :vcr do
-    user = create(:user, oauth_token: "tokentime")
+    user = create(:user, oauth_token: ENV["GITHUB_TOKEN"])
     token = user.oauth_token
 
     allow_any_instance_of(ApplicationController)
@@ -13,7 +13,7 @@ describe GithubService do
   end
 
   it 'gets repos by user', :vcr do
-    user = create(:user, first_name: "Wanda", oauth_token: "token")
+    user = create(:user, first_name: "Wanda", oauth_token: ENV["GITHUB_TOKEN"])
     token = user.oauth_token
 
     allow_any_instance_of(ApplicationController)
@@ -21,6 +21,7 @@ describe GithubService do
     github_service = GithubService.new(token)
 
     repo = github_service.find_repos.first
+    # binding.pry
     expect(github_service.find_repos.count).to eq(5)
     expect(repo).to have_key(:name)
     expect(repo).to have_key(:html_url)
