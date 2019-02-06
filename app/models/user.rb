e6d3a9a61a25c92c06066b96d09d684fc75535e2
Leah Miller <lkmiller.87@gmail.com>
2019-02-05 19:55:42 -0700

    Comments out from_omniauth method for refactor

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,15 @@ class User < ApplicationRecord
   has_many :videos, through: :user_videos
   has_many :friendships
   has_many :friends, through: :friendships
-  
+
   validates :email, uniqueness: true, presence: true
   validates_presence_of :password, if: :password
   validates_presence_of :first_name
   enum role: [:default, :admin]
   has_secure_password
 
-  def self.from_omniauth(auth_info, current_user)
-    current_user.update!(uid: auth_info[:uid], oauth_token: auth_info[:credentials][:token])
-    current_user
-  end
+  # def from_omniauth(auth_info)
+  #   update!(oauth_token: auth_info[:credentials][:token])
+  #   update!(uid: auth_info[:uid])
+  # end
 end
