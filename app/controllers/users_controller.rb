bafbcd8e3c9e951eada1a91bd2ab3eaccedd8596
Leah Miller <lkmiller.87@gmail.com>
2019-02-07 21:19:13 -0700

    Updates flash message'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,12 +9,12 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.create(user_params)
-    if user.save
-      session[:user_id] = user.id
+    @user = User.create(user_params)
+    if @user.save
+      session[:user_id] = @user.id
       redirect_to dashboard_path
     else
-      flash[:error] = 'Username already exists'
+      flash[:error] = 'Email already exists'
       render :new
     end
   end
